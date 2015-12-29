b73942a709818bdc8bbccdd41067f3b61a8c578f
ToniRib <tonimarierib@gmail.com>
2015-12-29 13:45:53 -0700

    updates through i16

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -9,6 +9,10 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
 
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://toni-rib-catch-of-the-day.firebaseio.com/');
+
 // App
 var App = React.createClass({
   getInitialState: function() {
@@ -16,6 +20,25 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+
+  componentDidMount: function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context: this,
+      state: 'fishes'
+    });
+
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
 
   addToOrder: function(key) {
@@ -144,7 +167,7 @@ var Order = React.createClass({
     }
 
     return (
-      <li>
+      <li key={key}>
         <span>{count}</span>lbs
         {fish.name}
         <span className="price">{h.formatPrice(count * fish.price)}</span>

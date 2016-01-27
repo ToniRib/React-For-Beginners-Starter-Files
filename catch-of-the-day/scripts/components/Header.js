b3c683229ed9504d65a3b64a60b92208303f85a6
ToniRib <tonimarierib@gmail.com>
2016-01-27 15:46:09 -0700

    Update Header to use ES6

--- catch-of-the-day/scripts/components/Header.js
+++ catch-of-the-day/scripts/components/Header.js
@@ -2,9 +2,8 @@
 
 import React from 'react';
 
-var Header = React.createClass({
-
-  render: function() {
+class Header extends React.Component {
+  render() {
     return (
       <header className="top">
         <h1>Catch
@@ -16,11 +15,11 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
-  },
-
-  propTypes: {
-    tagline: React.PropTypes.string.isRequired
   }
-});
+}
+
+Header.propTypes = {
+  tagline: React.PropTypes.string.isRequired
+}
 
 export default Header;
